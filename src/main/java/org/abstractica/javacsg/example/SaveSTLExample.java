f0c34af22d4b5ad7aa96687410d36333381d5050
tgrundtvig <TGrundtvig@gmail.com>
2023-09-06 09:04:32 +0200

    SaveSTLExample

---
+++ src/main/java/org/abstractica/javacsg/example/SaveSTLExample.java
@@ -0,0 +1,27 @@
+package org.abstractica.javacsg.example;
+
+import org.abstractica.javacsg.Geometry3D;
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
+
+import java.io.IOException;
+
+public class SaveSTLExample
+{
+	public static void main(String[] args) throws IOException
+	{
+		JavaCSG csg = JavaCSGFactory.createDefault();
+		Geometry3D torus = csg.torusSegment3D
+			(
+				10,
+				40,
+				csg.degrees(43),
+				csg.degrees(132),
+				16,
+				64,
+				true
+			);
+		csg.view(torus);
+		csg.saveSTL("C:/tmp/Torus.stl", torus);
+	}
+}
